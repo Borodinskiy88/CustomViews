c2cfe2d97f31b6b55d0b3c029f7f00890ee368d6
Borodinskiy88 <a-borodinskiy@mail.ru>
2023-03-29 19:13:20 +0300

    Initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+o
+?app/src/main/java/ru/netology/customviews/utils/AndroidUtils.kt,e\c\ec95c95d397f2af788085c46cdebe6d694ae115e
+i
+9app/src/main/java/ru/netology/customviews/ui/StatsView.kt,d\5\d5cfccf0cc33004e53e752770efbeae10e13a170
+i
+9app/src/main/java/ru/netology/customviews/MainActivity.kt,a\b\ab351579c3455851a48111734d015b8cc8d7fcdc
---
+++ app/src/main/java/ru/netology/customviews/MainActivity.kt
@@ -0,0 +1,22 @@
+package ru.netology.customviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.netology.customviews.ui.StatsView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<StatsView>(R.id.statsView).data = listOf(
+            500F,
+            500F,
+            500F,
+            500F,
+            500F,
+            200F,
+            200F,
+            200F,
+        )
+    }
+}
